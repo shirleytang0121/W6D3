dc69cc303cf8ab85f5c14f465d087e4eaa455324
Shirley Tang <shirleytang1996@gmail.com>
2021-06-16 16:42:57 -0400

    add routes

--- banana/config/routes.rb
+++ banana/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :users
+
+  get "/user", to: "user#index", as: "user"
+  post "/user", to: "user#create", as: "new_user"
+  
+
 end
