30fc97ac9ca60fb0da0a768bb9054a78e3abfae8
canberko560 <canberkvarli@gmail.com>
2021-06-16 16:39:17 -0700

    finish CRUD for users controller

--- banana/app/controllers/users_controller.rb
+++ banana/app/controllers/users_controller.rb
@@ -1,24 +1,53 @@
 class UsersController < ApplicationController
 
     def index
-        # render plain: "I'm the iron-man"
+        @users = User.all
+
+        render json: @users
     end
 
     def create
-        render json: params
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
     end
 
     def show
-        render plain: "Hi"
-
+        # # render plain: "Hi"
+        @user = User.find(params[:id])
+        render json: @user
     end
 
     def update
+        @user = User.find(params[:id])
+
+        if @user.update!(user_params)
+            render json: @user
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+
     end
 
-    def delete
+    def destroy
+        @user = User.find(params[:id])
+
+        @user.destroy
+            redirect_to users_url
+        
+
     end
 
 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+    
 
 end
