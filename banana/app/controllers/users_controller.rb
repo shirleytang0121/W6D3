a02be4c2cf9c1e7daf383ced5a418d7dee6a7075
canberko560 <canberkvarli@gmail.com>
2021-06-16 14:25:07 -0700

    fix initial errors

---
+++ banana/app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+
+    def index
+        render plain: "I'm the iron-man"
+    end
+
+end
--- banana/config/routes.rb
+++ banana/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # resources :users
+  resources :users
 
-  get "/user", to: "user#index", as: "user"
-  post "/user", to: "user#create", as: "new_user"
+  # get "/user", to: "user#index"
+  # post "/user", to: "user#create", as: "new_user"
+  # get "/user", to: "user#new"
+  # get "/user/:id", to: "user#edit"
+  # get "/user/:id", to: "user#show"
+  # patch "/user/:id", to: "user#update"
+  # delete "/user/:id", to: "user#destroy"
   
-
 end
