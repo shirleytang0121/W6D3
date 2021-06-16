abb39e40e26446a41c2a10734a5fde169315371a
Shirley Tang <shirleytang1996@gmail.com>
2021-06-16 17:56:53 -0400

    params

--- banana/app/controllers/users_controller.rb
+++ banana/app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
 
     def index
-        render plain: "I'm the iron-man"
+        # render plain: "I'm the iron-man"
     end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render plain: "Hi"
+
+    end
+
+    def update
+    end
+
+    def delete
+    end
+
+
 
 end
--- banana/config/routes.rb
+++ banana/config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 
-  # get "/user", to: "user#index"
-  # post "/user", to: "user#create", as: "new_user"
-  # get "/user", to: "user#new"
-  # get "/user/:id", to: "user#edit"
-  # get "/user/:id", to: "user#show"
-  # patch "/user/:id", to: "user#update"
-  # delete "/user/:id", to: "user#destroy"
+  # get "/users", to: "users#index"
+  # post "/users", to: "users#create", as: "new_user"
+  # get "/users", to: "users#new"
+  # get "/users/:id", to: "users#edit"
+  # get "/users/:id", to: "users#show"
+  # patch "/users/:id", to: "users#update"
+  # delete "/users/:id", to: "users#destroy"
   
 end
