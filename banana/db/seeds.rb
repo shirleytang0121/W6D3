3d909b574cd71634bd13a272d144cf555bcc2871
canberko560 <canberkvarli@gmail.com>
2021-06-16 17:32:57 -0700

    add models and create data

--- banana/db/seeds.rb
+++ banana/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    users = User.create([{username: 'shirley99'}, {username: 'canberko560'}, {username: 'whatswrongandy'}, {username: 'Leonardo DaVinci'}])
+    Artwork.create(title: 'Mona lisa', image_url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMona_Lisa&psig=AOvVaw29ops3mNWWxW_d2m-n_4B4&ust=1623976218694000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMCexoC1nfECFQAAAAAdAAAAABAI', artist_id: users.first.id)
